a02d7cca8134b39ba3087a49d3071180a6b4ae39
oca-john <40886062+oca-john@users.noreply.github.com>
2020-06-27 22:30:16 +0800

    Update Python3_Practice.py

--- Python3_Practice.py
+++ Python3_Practice.py
@@ -4,7 +4,7 @@
 name = input("please input your name")  # 提示用户输入内容，并赋值给name变量
 print("your name is:",name)             # 通过print函数打印结果
 
-# format output - %占位符
+# format output - %占位符格式化
 name = 'zhang'                          # 定义内容变量
 age = 29
 print("your name is %s, age is %i!" %(name, age))       # 文本区域内用%s/%i代替可变区，变量区域内用%()放置所有变量
