701e4aadaa7c5723ac7cf588b08434b681e577d8
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-09 18:07:27 +0800

    Update 1.python3.files.open.read.write.close.py

--- Python3Codes/1.python3.files.open.read.write.close.py
+++ Python3Codes/1.python3.files.open.read.write.close.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 # python3.files.open.read.write.close
 
-# 
+# open file
+f = open("/home/user/documents/filename.txt", "r")  # open文件给句柄f
+# read content of file
+str = f.read()                                      # 用句柄调用文件，用read()函数读取内容给变量str
+print(str)                                          # 打印变量str内容
+# close file
+f.close()                                           # f.close()关闭句柄
+
+# open file
+f = open("/home/user/documents/filename.txt", "r")  # open文件给句柄f
+# write content into file
+f.write("python is useful, I'll learn it.\n")       # f.write()写入内容
+# close file
+f.close()                                           # 关闭句柄
