8b43027bf3264369acab20f9019496a5e01ba4e8
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-09 16:59:55 +0800

    Create 1.python3.map.func.py

---
+++ Python3Codes/1.python3.map.func.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# map function is used to map each element in an array run in a certain function
+# map函数调用指定函数计算数组中的每个元素的值
+
+# def a calculating function, 定义一个计算函数
+def sqr(x):
+
+# use map function, 用map函数遍历数组，并用上述函数计算
+
