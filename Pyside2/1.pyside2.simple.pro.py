f7fd44f4f140679a20bbbffc6a28de384635693a
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-10 09:19:39 +0800

    Create 1.pyside2.simple.pro.py

---
+++ Pyside2/1.pyside2.simple.pro.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import PySide2.QtCore           # 此案例只用的"qt.core部分"
+from PySide2.QtWidgets import QApplication, QLabel  # 具体用到的是qtwidgets部件中的"Q程序和Q标签"
+
+print(PySide2.__version__)      # 查看PySide2的版本信息，和torh.__version__类似
+
+app = QApplication()            # 初始化Q程序类的一个实例，命名为app
+label = QLabel('Hello world!')  # 为程序添加部件Qlabel标签对象，命名为label
+label.show()                    # 显示label标签对象
+app.exec_()                     # 程序退出时结束
