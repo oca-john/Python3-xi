992efbde533a09fb1253c5d0217b9dfae51ac484
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-10 09:31:49 +0800

    Update 1.pyside2.2.signal.groove.py

--- Pyside2/1.pyside2.2.signal.groove.py
+++ Pyside2/1.pyside2.2.signal.groove.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 
-import 
+# 连接信号与槽的代码
+# element.signal_name.connect(slot_name)
+# 某element（对象）.的signal（事件函数）.connect到某个槽（执行函数）
+
+from PySide2 import QtWidgets       # 导入窗体部件
+
+app = QtWidgets.QApplication()      # 初始化一个Q程序的实例，命名为app
+
+btn_quit = QtWidgets.QPushButton("Quit")    # 创建一个按钮（显示Quit），命名为btn_quit
+
+btn_quit.resize(100, 80)            # 设置btn_quit对象的size属性，用resize重置其窗口大小
+
+btn_quit.clicked.connect(app.quit)  # 将btn_quit对象的click事件，连接到app实例的quit执行函数
+
+btn_quit.show()                     # 定义完成后，显示该按钮
+
+app.exec_()                         # 运行该程序
