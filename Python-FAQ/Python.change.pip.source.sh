6decec9cf2d64d58342f3d4227d727fd8749e929
oca-john <40886062+oca-john@users.noreply.github.com>
2021-02-08 16:02:26 +0800

    Update Python.change.pip.source.sh

--- Python-FAQ/Python.change.pip.source.sh
+++ Python-FAQ/Python.change.pip.source.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sh
 
 # 临时使用国内pip源
-pip install pkgs -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install pkgs -i https://mirrors.ustc.edu.cn/pypi/web/simple
 
 # 永久修改为国内pip源
 pip install pip -U pip    (实际上等同于：pip install pip --upgrade pip)
-pip config set global.source https://pypi.tuna.tsinghua.edu.cn/simple
+pip config set global.source https://mirrors.ustc.edu.cn/pypi/web/simple
